f62eec96bf23ef821a15078f42751e58ff1b66cd
Brierre Titouan <tbrierre@iutbayonne.univ-pau.fr>
2022-04-23 18:56:35 +0200

    renommer et reorganiser les fichiers du semestre 2

--- SEMESTRE-2/Ressources/stats/pandas.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy
-import matplotlib.pyplot as plt
-
-Pic=pd.read_table('F:\BUT\SEMESTRE-2\stats\Pic.csv',sep=";", decimal=',', encoding='ANSI')
-
-Pic['Age'].plot(kind="hist",bins=6)
-
-plt.show()
